08562d122d9899cdb32bfe1075a7643611648ecc
AfridShaik <afridali102@gmail.com>
2022-03-19 13:06:28 +0530

    tost meaagae

---
+++ src/app/Mycomponent/todo-item/todo-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+import {NgToastService} from 'ng-angular-popup'
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input()
+  todo!: Todo;
+  
+  constructor(private tost: NgToastService) { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(){
+    this.tost.warning({detail:"Deleted",summary:"Your todo list has been deleted",duration:2000})
+    console.log("Onlick has been triggred")
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './Mycomponent/todo/todo.component';
+import { TodoItemComponent } from './Mycomponent/todo-item/todo-item.component';
+import { NgToastModule } from 'ng-angular-popup';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    TodoItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
