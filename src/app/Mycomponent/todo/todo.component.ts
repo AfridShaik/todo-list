b565734689c17061e2125165fefceb9d051a8ecd
AfridShaik <afridali102@gmail.com>
2022-03-19 12:04:52 +0530

    addes list

---
+++ src/app/Mycomponent/todo/todo.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  @Input()  todos: Todo[] ; 
+  
+  constructor() {
+    this.todos = [
+      {
+        sno:1,
+        title: "This 1 is title",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno:2,
+        title: "This 2 is title",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno:3,
+        title: "This 3 is title",
+        desc: "Description",
+        active: true
+      }
+    ]
+  }
+
+  
+
+  ngOnInit(): void {
+  }
+
+}
