2a183d78f995435fcec5c2ec50746b3552d130a2
AfridShaik <afridali102@gmail.com>
2022-03-22 11:00:35 +0530

    Done adding home and home button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './Mycomponent/about/about.component';
+import { HomeComponent } from './Mycomponent/home/home.component';
 import { TodoComponent } from './Mycomponent/todo/todo.component';
 
 const routes: Routes = [
-  { path: '', component: TodoComponent },
+  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'todo', component: TodoComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
